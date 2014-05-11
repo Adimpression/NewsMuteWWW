701d1300b8122f9b910180a3065daf36e3514685
Ravindranath Akila <ravindranathakila@gmail.com>
2014-05-11 13:45:38 +0800

    http://NewsMute.com optimizing #newsmute-133 In Progress

--- js/index.js
+++ js/index.js
@@ -569,7 +569,7 @@ function just_visiting() {
 
 function post_session(){
     d(post_session);
-    try { //initialSetup();
+    try { //intent_initial_setup();
         WakeUp();
         just_visiting();
 
@@ -586,46 +586,6 @@ function post_session(){
         if(debug){
             alert("post_session:" + e);
         }
-    }
-}
-
-
-var spinner =  spinner = new Spinner({
-    lines: 17, // The number of lines to draw
-    length: 0, // The length of each line
-    width: 2, // The line thickness
-    radius: 24, // The radius of the inner circle
-    corners: 1, // Corner roundness (0..1)
-    rotate: 0, // The rotation offset
-    direction: 1, // 1: clockwise, -1: counterclockwise
-    color: '#aaa', // #rgb or #rrggbb or array of colors
-    speed: 1.3, // Rounds per second
-    trail: 100, // Afterglow percentage
-    shadow: false, // Whether to render a shadow
-    hwaccel: false, // Whether to use hardware acceleration
-    className: 'spinner', // The CSS class to assign to the spinner
-    zIndex: 2e9, // The z-index (defaults to 2000000000)
-    top: 'auto', // Top position relative to parent in px
-    left: 'auto' // Left position relative to parent in px
-}).spin(document.getElementById('Busy'));
-
-function busy(){
-    try {
-        spinner.spin(document.getElementById('Busy'));
-        section($Busy);
-    } catch (e) {
-        if(debug){
-            alert(e);
-        }
-    }
-}
-
-function free(){
-    try {
-        spinner.stop();
-        //spinner = null;
-    } catch (e) {
-        alert(e);
     }
 }
 
@@ -1408,7 +1368,7 @@ function isConnected() {
 }
 
 
-function initialSetup(){
+function intent_initial_setup(){
     try {
         clearTimeout(feedRefreshTimeout);
 
@@ -1513,9 +1473,39 @@ function initialSetup(){
     }
 }
 
-function launchInception(){
-    clearInterval(feedRefreshTimeout);
-    section($Inception);
+
+const spinner =  spinner = new Spinner({
+    lines: 17, // The number of lines to draw
+    length: 0, // The length of each line
+    width: 2, // The line thickness
+    radius: 24, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#aaa', // #rgb or #rrggbb or array of colors
+    speed: 1.3, // Rounds per second
+    trail: 100, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+}).spin(document.getElementById('Busy'));
+
+function busy(){
+    f(spinner.spin)(document.getElementById('Busy'));
+    f(section)($Busy);
+}
+
+function free(){
+    f(spinner.stop);
+}
+
+
+function render_inception(){
+    f(clearInterval)(feedRefreshTimeout);
+    f(section)($Inception);
 }
 
 function section(sectionToShow) {
