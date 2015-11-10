1c9d634dd5a04db2164b0c6e3cd7ea15573fd0c0
Ravindranath Akila <ravindranathakila@gmail.com>
2015-11-10 14:45:41 +0800

    URL validation function added

--- js/utility.js
+++ js/utility.js
@@ -56,7 +56,123 @@ angular.module('app.utility', [])
             $http.defaults.headers.common["x-session-header"] = "";
         };
 
+        root.isValidURL = (function () {// wrapped in self calling function to prevent global pollution
+
+            //URL pattern based on rfc1738 and rfc3986
+            var rg_pctEncoded = "%[0-9a-fA-F]{2}";
+            var rg_protocol = "(http|https):\\/\\/";
+
+            var rg_userinfo = "([a-zA-Z0-9$\\-_.+!*'(),;:&=]|" + rg_pctEncoded + ")+" + "@";
+
+            var rg_decOctet = "(25[0-5]|2[0-4][0-9]|[0-1][0-9][0-9]|[1-9][0-9]|[0-9])"; // 0-255
+            var rg_ipv4address = "(" + rg_decOctet + "(\\." + rg_decOctet + "){3}" + ")";
+            var rg_hostname = "([a-zA-Z0-9\\-\\u00C0-\\u017F]+\\.)+([a-zA-Z]{2,})";
+            var rg_port = "[0-9]+";
+
+            var rg_hostport = "(" + rg_ipv4address + "|localhost|" + rg_hostname + ")(:" + rg_port + ")?";
+
+            // chars sets
+            // safe           = "$" | "-" | "_" | "." | "+"
+            // extra          = "!" | "*" | "'" | "(" | ")" | ","
+            // hsegment       = *[ alpha | digit | safe | extra | ";" | ":" | "@" | "&" | "=" | escape ]
+            var rg_pchar = "a-zA-Z0-9$\\-_.+!*'(),;:@&=";
+            var rg_segment = "([" + rg_pchar + "]|" + rg_pctEncoded + ")*";
+
+            var rg_path = rg_segment + "(\\/" + rg_segment + ")*";
+            var rg_query = "\\?" + "([" + rg_pchar + "/?]|" + rg_pctEncoded + ")*";
+            var rg_fragment = "\\#" + "([" + rg_pchar + "/?]|" + rg_pctEncoded + ")*";
+
+            var rgHttpUrl = new RegExp(
+                "^"
+                + rg_protocol
+                + "(" + rg_userinfo + ")?"
+                + rg_hostport
+                + "(\\/"
+                + "(" + rg_path + ")?"
+                + "(" + rg_query + ")?"
+                + "(" + rg_fragment + ")?"
+                + ")?"
+                + "$"
+            );
+
+            // export public function
+            return function (url) {
+                return rgHttpUrl.test(url);
+            };
+        })();
+
+
+
+        root.d = function(alertText) {
+            if (debug) {
+                try {
+                    //alert(alertText);
+                    notifyShort(alertText);
+                } catch (e) {
+                    alert(alertText);//In case the toast plugin fails
+                }
+            }
+            return alertText;
+        };
+
+        root.f = function(fun) {
+            return function () {
+                if (debug) {
+                    try {
+                        return fun.apply(this, arguments);
+                    } catch (e) {
+                        root.d(arguments.callee.caller.toString() + "\n encountered an error invoking a function \n" + "\nDetails as follows:\n" + e +"\nFunction:\n" + (fun ? functionName(fun) : 'undefined'));
+                        return null;
+                    }
+                } else {
+                    return fun.apply(this, arguments);
+                }
+            };
+        };
+
+        root.j = function(alertJSON) {
+            if (debug) {
+                try {
+                    notifyShort(JSON.stringify(alertJSON));
+                } catch (e) {
+                    alert(JSON.stringify(alertJSON));//In case the toast plugin fails
+                }
+            }
+            return alertJSON;
+        };
+
+
+        root.functionName = function(fun) {
+            var ret = fun.toString();
+            ret = ret.substr('function '.length);
+            ret = ret.substr(0, ret.indexOf('('));
+            return ret;
+        };
+
+
+        root.getUrlParameter = function (name) {
+            name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+            var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+                results = regex.exec(location.search);
+            return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+        };
+
+
+        root.intent_open_link = function(link) {
+            //if(currentFeedItems <= 11){
+            //    intent_subscribe_silently_if_valid_feed(link, function(){
+            //        d("Subscribe failed on auto follow mode");
+            //    });
+            //}
+
+            window.open(link, '_system', 'location=yes');
+        };
+
+        root.safeApply = function(scope, fn) {
+            (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
+        };
 
         return root;
     });
+
 
