fcbdfb9e4730a70dfa644740bbf3a19d43f28ab6
Ravindranath Akila <ravindranathakila@gmail.com>
2016-01-02 14:47:22 +0800

    Trivial

--- js/services.js
+++ js/services.js
@@ -15,49 +15,49 @@ angular.module('app.services', [])
                 method: 'GET',
                 url: REGISTER_URL + "user=" + username + "&token=" + password + "&nmact=CREATE&email=" + email
             });
-        }
+        };
 
         this.login = function (username, password) {
             return $http({
                 method: 'GET',
                 url: LOGIN_URL + "user=" + username + "&token=" + password + "&nmact=READ"
             });
-        }
+        };
 
         this.subscribeFeed = function (username, url) {
             return $http({
                 method: 'GET',
                 url: SUBSCRIBE_URL + "user=" + username + "&url=" + url + "&nmact=CREATE"
             });
-        }
+        };
 
         this.newsFeed = function (username) {
             return $http({
                 method: 'GET',
                 url: NEWS_FEED_URL + "user=" + username + "&nmact=READ"
             });
-        }
+        };
 
         this.readNews = function (username, url) {
             return $http({
                 method: 'GET',
                 url: NEWS_FEED_URL + "user=" + username + "&nmact=DELETE&url=" + url
             });
-        }
+        };
 
         this.muteNews = function (username, url) {
             return $http({
                 method: 'GET',
                 url: NEWS_FEED_URL + "user=" + username + "&nmact=DELETE&url=" + url
             });
-        }
+        };
 
         this.shareNews = function (username, url) {
             return $http({
                 method: 'GET',
                 url: NEWS_SHARE_URL + "user=" + username + "&url=" + url
             });
-        }
+        };
 
         this.getUserLocation = function(){
 
