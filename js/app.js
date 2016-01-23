ce701c02c761d72c0d967a26fd448c31e722849b
Ravindranath Akila <ravindranathakila@gmail.com>
2016-01-23 14:49:41 +0800

    Debugging login error on app

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ angular.module('app.newsmute', ['ionic', 'app.controllers', 'app.factory', 'app.
             if (window.cordova && window.cordova.plugins.Keyboard) {
                 cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
             }
+
             if (window.StatusBar) {
                 // org.apache.cordova.statusbar required
                 StatusBar.styleDefault();
