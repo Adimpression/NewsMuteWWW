b1e7fa4165327b7ec85eb1372d774922fad9474f
Ravindranath Akila <ravindranathakila@gmail.com>
2016-01-24 17:43:29 +0800

    Cognito: Testing Google Oauth 2.0

--- js/controllers.js
+++ js/controllers.js
@@ -35,12 +35,10 @@ angular.module('app.controllers', ['angular-hmac-sha512', 'app.utility'])
     .controller('LoginCtrl', function ($scope, $state, $rootScope, $log, AppService, Utility) {
 
         if (typeof String.prototype.startsWith != 'function') {
-            String.prototype.startsWith = function (str){
+            String.prototype.startsWith = function (str) {
                 return this.indexOf(str) == 0;
             };
         }
-
-
 
 
         //AWS.config.region = 'us-east-1';
@@ -98,13 +96,9 @@ angular.module('app.controllers', ['angular-hmac-sha512', 'app.utility'])
             var ref = window.open('https://accounts.google.com/o/oauth2/auth?client_id=' + clientId + '&redirect_uri=http://localhost/callback&scope=https://www.googleapis.com/auth/urlshortener&approval_prompt=force&response_type=code&access_type=offline', '_blank', 'location=yes');
             ref.addEventListener('loadstart', function (event) {
                 try {
-                    if ((event.url).startsWith("http://localhost/callback")) {
-                        requestToken = (event.url).split("code=")[1];
-                        alert(requestToken);
-                        ref.close();
-                    } else {
-                        alert('No start');
-                    }
+                    requestToken = (event.url).split("code=")[1];
+                    ref.close();
+                    alert(requestToken);
                 } catch (e) {
                     alert(e);
                 }
