1309fb2f09507a392cb9859bce4a733bc9f0796c
Ravindranath Akila <ravindranathakila@gmail.com>
2016-03-13 18:30:18 +0800

    Lambda functions with TypeScript. Works on AWS Lambda (js) file.

---
+++ js/lambda/superfriend.ts
@@ -0,0 +1,55 @@
+/// <reference path="node" />
+/// <reference path="lambda.d.ts" />
+
+console.log('Loading function');
+
+var doc = require('dynamodb-doc');
+var dynamo = new doc.DynamoDB();
+
+/**
+ * Provide an event that contains the following keys:
+ *
+ *   - operation: one of the operations in the switch statement below
+ *   - tableName: required for operations that interact with DynamoDB
+ *   - payload: a parameter to pass to the operation being performed
+ */
+exports.handler = function (event:Object, context:Context) {
+    console.log('Received Event:', JSON.stringify(event, null, 2));
+    console.log('Received Context:', JSON.stringify(context));
+    console.log("Cognito Identity:", JSON.stringify(context.identity));
+
+    JSON.parse(event.query.events).forEach(function (action) {
+        "use strict";
+        var operation = action.operation;
+
+        if (action.tableName) {
+            action.payload.TableName = action.tableName;
+        }
+
+        switch (operation) {
+            case 'create':
+                dynamo.putItem(action.payload, context.done);
+                break;
+            case 'read':
+                dynamo.getItem(action.payload, context.done);
+                break;
+            case 'update':
+                dynamo.updateItem(action.payload, context.done);
+                break;
+            case 'delete':
+                dynamo.deleteItem(action.payload, context.done);
+                break;
+            case 'list':
+                dynamo.scan(action.payload, context.done);
+                break;
+            case 'echo':
+                context.succeed(action.payload);
+                break;
+            case 'ping':
+                context.succeed('pong');
+                break;
+            default:
+                context.fail(new Error('Unrecognized operation "' + operation + '"'));
+        }
+    });
+};
