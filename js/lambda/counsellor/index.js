db7352e5b54e9693f25eca5d3650436055f76243
Ravindranath Akila <ravindranathakila@gmail.com>
2016-04-10 14:21:27 +0800

    Insert data into Yawn

--- js/lambda/counsellor/index.js
+++ js/lambda/counsellor/index.js
@@ -22,28 +22,34 @@ exports.handler = function (event, context) {
 
         console.log(JSON.stringify(record));
 
+
         switch (record.eventName) {
-            case 'REMOVE':
-                request(record.dynamodb.OldImage.ref.S, function (error, response, html) {
-                    if (!error && response.statusCode == 200) {
-
-                        var $ = cheerio.load(html);
-                        console.log('Title:' + $('title'));
-                        console.log('Description:' + $('meta[name=description]').attr("content"));
-                        callback();
-                    } else {
-                        callback('HTTP Status Code:' + response.statusCode);
-                    }
-                });
-                break;
             case "INSERT":
-                request(record.dynamodb.NewImage.ref.S, function (error, response, html) {
-                    if (!error && response.statusCode == 200) {
 
+                var me = record.dynamodb.NewImage.me.S;
+                var ref = record.dynamodb.NewImage.ref.S;
+
+                request(ref, function (error, response, html) {
+                    if (!error && response.statusCode == 200) {
                         var $ = cheerio.load(html);
-                        console.log('Title:' + $('title'));
-                        console.log('Description:' + $('meta[name=description]').attr("content"));
-                        callback();
+                        var title = $('title').text();
+                        var content = $('meta[name=description]').attr("content");
+                        console.log('Title:' + title);
+                        console.log('Description:' + content);
+
+
+                        var itemToBeInserted = {
+                            'TableName': 'Yawn',
+                            'Item': {
+                                'me': me,
+                                'ref': title
+                            }
+                        };
+
+                        console.log(itemToBeInserted);
+
+                        dynamo.putItem(itemToBeInserted, callback);
+
                     } else {
                         callback('HTTP Status Code:' + response.statusCode);
                     }
