86caf4cce3ba2bef81decf02d8fb13caeb3e6c97
Ravindranath Akila <ravindranathakila@gmail.com>
2016-04-15 15:46:01 +0800

    Prepared Stalk code. Need to test it on Lambda

---
+++ js/lambda/stalk/index.js
@@ -0,0 +1,58 @@
+console.log('Starting to Scream');
+
+var doc = require('dynamodb-doc');
+
+
+var dynamo = new doc.DynamoDB();
+
+exports.handler = function (event, context) {
+    console.log('event:', JSON.stringify(event));
+    console.log('context:', JSON.stringify(context));
+
+    var events;
+
+    switch (event.method) {
+        case 'GET':
+            events = event.query.events;
+            break;
+        case 'POST':
+            events = event.body.events;
+    }
+
+
+    JSON.parse(events).forEach(function (action) {
+            "use strict";
+            var operation = action.operation;
+
+
+            switch (operation) {
+                case 'create':
+                    action.payload.forEach(function (item) {
+                        dynamo.putItem(
+                            {
+                                'TableName': 'Stalk',
+                                'Item': {
+                                    'me': context.identity.cognitoIdentityId,
+                                    'ref': item
+                                }
+                            }
+                            , context.done);
+                    });
+                    break;
+                case 'list':
+                    dynamo.query(
+                        {
+                            'TableName': 'Stalk',
+                            'KeyConditionExpression': "me = :me",
+                            'ExpressionAttributeValues': {
+                                ':me': context.identity.cognitoIdentityId
+                            }
+                        }
+                        , context.done);
+                    break;
+                default:
+                    context.fail(new Error('Unrecognized operation "' + operation + '"'));
+            }
+        }
+    );
+};
