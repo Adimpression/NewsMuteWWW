5dca34b9c3b7da0a0076ba74ba9e988a4b85ad1e
Ravindranath Akila <ravindranathakila@gmail.com>
2016-04-16 01:19:08 +0800

    DynamoDB querying based on Cognito event done (from Stalk).

--- js/lambda/harvest/index.js
+++ js/lambda/harvest/index.js
@@ -9,25 +9,24 @@ exports.handler = function (event, context) {
     console.log('event:', JSON.stringify(event));
     console.log('context:', JSON.stringify(context));
 
-    context.done(null, event);
-
-    // dynamo.query(
-    //     {
-    //         'TableName': 'Stalk',
-    //         'KeyConditionExpression': "me = :me",
-    //         'ExpressionAttributeValues': {
-    //             ':me': context.identity.cognitoIdentityId
-    //         }
-    //     }, function (result) {
-    //         console.log(JSON.stringify(result));
-    //         // dynamo.putItem(
-    //         //     {
-    //         //         'TableName': 'Yawn',
-    //         //         'Item': {
-    //         //             'me': context.identity.cognitoIdentityId,
-    //         //             'ref': item
-    //         //         }
-    //         //     }
-    //         //     , context.done);
-    //     });
+    dynamo.query(
+        {
+            'TableName': 'Stalk',
+            'KeyConditionExpression': "me = :me",
+            'ExpressionAttributeValues': {
+                ':me': event.identityId
+            }
+        }, function (hmmWhatsThis, goodStuff, hmmmNoIdea) {
+            console.log(JSON.stringify(goodStuff));
+            context.done(null, event);
+            // dynamo.putItem(
+            //     {
+            //         'TableName': 'Yawn',
+            //         'Item': {
+            //             'me': context.identity.cognitoIdentityId,
+            //             'ref': item
+            //         }
+            //     }
+            //     , context.done);
+        });
 };
