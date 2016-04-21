e56d7014e7e159041070f7b0936e5383789dd35f
Ravindranath Akila <ravindranathakila@gmail.com>
2016-04-22 02:01:31 +0800

    Working async callbacks without dynamo db insert

--- js/lambda/harvest/index.js
+++ js/lambda/harvest/index.js
@@ -4,6 +4,7 @@ var doc = require('dynamodb-doc');
 var request = require('request');
 var FeedParser = require('feedparser');
 var async = require('async');
+var _ = require('highland');
 var parse = require('./ts/Parse');
 
 
@@ -21,90 +22,92 @@ exports.handler = function (event, context) {
                 ':me': event.identityId
             }
         }, function (error, dataFromStalk) {
-            async.mapSeries(new parse.Parse().rootObject(dataFromStalk).Items, function (element, callback) {
+            console.log(JSON.stringify(dataFromStalk));
+            _(new parse.Parse().rootObject(dataFromStalk).Items)
+                .flatFilter(
+                    function (element) {
+                        return _(function (pushFunc, next) {
 
-                var item = new parse.Parse().item(element);
+                            var item = new parse.Parse().item(element);
 
-                var req = request(item.ref);
-                var feedparser = new FeedParser();
+                            var req = request(item.ref);
+                            var feedparser = new FeedParser();
 
-                req.on('error', function (error) {
-                    console.log(error);
-                });
+                            req.on('error', function (error) {
+                                console.log(error);
+                            });
 
-                req.on('response', function (res) {
-                    var stream = this;
+                            req.on('response', function (res) {
+                                var stream = this;
 
-                    if (res.statusCode != 200) {
-                        return this.emit('error', new Error('Bad status code'));
-                    }
+                                if (res.statusCode != 200) {
+                                    return this.emit('error', new Error('Bad status code'));
+                                }
 
-                    stream.pipe(feedparser);
-                });
+                                stream.pipe(feedparser);
+                            });
 
-                feedparser.on('error', function (error) {
-                    console.log(error);
-                });
+                            feedparser.on('error', function (error) {
+                                console.log(error);
+                            });
 
-                feedparser.on('readable', function () {
-                    var stream = this;
-
-                    var streamedItem;
-
-                    async.whilst(function () {
-                            return (streamedItem = stream.read()) != null;
-                        },
-                        function (asyncCallback) {
-
-                            asyncCallback(null, streamedItem);
-
-                            // dynamo.query(
-                            //     {
-                            //         'TableName': 'Yawn',
-                            //         'KeyConditionExpression': 'me = :me and #ref = :moodref',
-                            //         'ExpressionAttributeNames': {
-                            //             '#ref': 'ref'
-                            //         },
-                            //         'ExpressionAttributeValues': {
-                            //             ':me': me,
-                            //             ':moodref': '0' + streamedItem.link
-                            //         }
-                            //     },
-                            //     function (error, data) {
-                            //         // console.log("error:" + JSON.stringify(error));
-                            //         // console.log("data:" + JSON.stringify(data));
-                            //         // console.log("hmmmNoIdea:" + JSON.stringify(hmmmNoIdea));
-                            //         asyncCallback(null, streamedItem);
-                            //
-                            //         // var rootObject = new parse.Parse().rootObject(data);
-                            //         // var presentInAlreadyReadItems = rootObject.Items.length != 0;
-                            //         //
-                            //         // if (!presentInAlreadyReadItems) {
-                            //         //     dynamo.putItem({
-                            //         //         'TableName': 'Yawn',
-                            //         //         'Item': {
-                            //         //             'me': me,
-                            //         //             'ref': '1' + streamedItem.link,
-                            //         //             'title': streamedItem.title,
-                            //         //             'content': streamedItem.description
-                            //         //         }
-                            //         //     }, function () {
-                            //         //         console.log("Inserted item into database");
-                            //         //         asyncCallback(null, streamedItem);
-                            //         //     });
-                            //         // } else {
-                            //         //     console.log("Ignoring dead item");
-                            //         //     asyncCallback(null, streamedItem);
-                            //         // }
-                            //     });
-                        },
-                        function (error, ignore) {
-                            callback(error, element);
-                        }
-                    );
-                });
-            }, function (err, results) {
-                context.done(null, event);
-            });
+                            feedparser.on('readable', function () {
+                                _(this)
+                                    .each(function (item) {
+                                        console.log('item:' + item.link);
+                                    })
+                                    .done(function () {
+                                        console.log('inner done');
+                                        pushFunc(null, true);
+                                    });
+                            });
+                        });
+                    })
+                .done(
+                    function (err, results) {
+                        console.log('outer done');
+                        context.done(null, event);
+                    });
         });
 };
+
+
+// dynamo.query(
+//     {
+//         'TableName': 'Yawn',
+//         'KeyConditionExpression': 'me = :me and #ref = :moodref',
+//         'ExpressionAttributeNames': {
+//             '#ref': 'ref'
+//         },
+//         'ExpressionAttributeValues': {
+//             ':me': me,
+//             ':moodref': '0' + streamedItem.link
+//         }
+//     },
+//     function (error, data) {
+//         // console.log("error:" + JSON.stringify(error));
+//         // console.log("data:" + JSON.stringify(data));
+//         // console.log("hmmmNoIdea:" + JSON.stringify(hmmmNoIdea));
+//         asyncCallback(null, streamedItem);
+//
+//         // var rootObject = new parse.Parse().rootObject(data);
+//         // var presentInAlreadyReadItems = rootObject.Items.length != 0;
+//         //
+//         // if (!presentInAlreadyReadItems) {
+//         //     dynamo.putItem({
+//         //         'TableName': 'Yawn',
+//         //         'Item': {
+//         //             'me': me,
+//         //             'ref': '1' + streamedItem.link,
+//         //             'title': streamedItem.title,
+//         //             'content': streamedItem.description
+//         //         }
+//         //     }, function () {
+//         //         console.log("Inserted item into database");
+//         //         asyncCallback(null, streamedItem);
+//         //     });
+//         // } else {
+//         //     console.log("Ignoring dead item");
+//         //     asyncCallback(null, streamedItem);
+//         // }
+//     });
