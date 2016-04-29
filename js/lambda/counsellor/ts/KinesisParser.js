624df41c8d8569eb3782e03bf773abc5557eb659
Ravindranath Akila <ravindranathakila@gmail.com>
2016-04-29 13:28:39 +0800

    Code cleanup

---
+++ js/lambda/counsellor/ts/KinesisParser.js
@@ -0,0 +1,15 @@
+///<reference path="DynamoDBEvent.ts"/>
+"use strict";
+var Parse = (function () {
+    function Parse() {
+    }
+    Parse.prototype.rootObject = function (rootObject) {
+        return rootObject;
+    };
+    Parse.prototype.record = function (record) {
+        return record;
+    };
+    return Parse;
+}());
+exports.Parse = Parse;
+//# sourceMappingURL=KinesisParser.js.map
