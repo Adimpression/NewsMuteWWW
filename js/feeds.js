c3a2eb7b005107bafa63542bca1a30fbd386b60c
ravindranathakila <ravindranathakila@gmail.com>
2017-04-02 11:23:51 +0800

    Added more feeds

--- js/feeds.js
+++ js/feeds.js
@@ -22,6 +22,34 @@ angular.module('app.feedurls', [])
             {
                 title: "CBS News                ",
                 feeds: ["http://www.cbsnews.com/latest/rss/main"]
+            },
+            {
+                title: "Time Most Popular       ",
+                feeds: ["http://feeds2.feedburner.com/time/topstories"]
+            },
+            {
+                title: "Time News Feed          ",
+                feeds: ["http://feeds.feedburner.com/time/newsfeed"]
+            },
+            {
+                title: "Time Ideas Feed         ",
+                feeds: ["http://feeds.feedburner.com/time/ideas"]
+            },
+            {
+                title: "Your Tango              ",
+                feeds: ["http://www.yourtango.com/rss/all-posts"]
+            },
+            {
+                title: "WHO                                    ",
+                feeds: ["http://www.who.int/feeds/entity/mediacentre/news/en/rss.xml"]
+            },
+            {
+                title: "WebMD                                  ",
+                feeds: ["http://rssfeeds.webmd.com/rss/rss.aspx?RSSSource=RSS_PUBLIC"]
+            },
+            {
+                title: "Science Daily - Health                 ",
+                feeds: ["https://rss.sciencedaily.com/top/health.xml"]
             }
         ];
 
